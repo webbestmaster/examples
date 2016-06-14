a56748f3fcf227c811b9c33333e9865ad904dbaa
webbestmaster <web.best.master@gmail.com>
2016-06-14 15:17:36 +0300

    mocha-js - FE

---
+++ mocha-js-front/cow.js
@@ -0,0 +1,19 @@
+// cow.js
+(function(exports) {
+	"use strict";
+
+	function Cow(name) {
+		this.name = name || "Anon cow";
+	}
+
+	exports.Cow = Cow;
+
+	Cow.prototype = {
+		greets: function(target) {
+			if (!target)
+				return console.error("missing target");
+			console.log(this.name + " greets " + target);
+		}
+	};
+
+})(this);
---
+++ mocha-js-front/cow_test.js
@@ -0,0 +1,39 @@
+var expect = chai.expect;
+var assert = chai.assert;
+
+describe("Cow", function() {
+	describe("constructor", function() {
+		it("should have a default name", function() {
+			var cow = new Cow();
+			expect(cow.name).to.equal("Anon cow");
+		});
+
+		it("should set cow's name if provided", function() {
+			var cow = new Cow("Kate");
+			expect(cow.name).to.equal("Kate");
+		});
+	});
+
+	describe("#greets", function() {
+		it("should throw if no target is passed in", function() {
+			expect(function() {
+				(new Cow()).greets();
+			}).to.throw(Error);
+		});
+
+		it("should greet passed target", function() {
+			var greetings = (new Cow("Kate")).greets("Baby");
+			expect(greetings).to.equal("Kate greets Baby");
+		});
+	});
+});
+
+
+describe('Array', function() {
+	describe('#indexOf()', function () {
+		it('should return -1 when the value is not present', function () {
+			assert.equal(-1, [1,2,3].indexOf(5));
+			assert.equal(-1, [1,2,3].indexOf(0));
+		});
+	});
+});
